94ed456ac7da0bd6dd98ac09b45430741e50e32b
Wes Duenow <wjduenow@raybeam.com>
2017-04-12 17:45:52 -0700

    Started Tests and added some fixtures

--- team_manager/tests.py
+++ team_manager/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Player, Group, GymSlot, GymSession, Game, Team, TeamPlayer, PlayerStats
 
-# Create your tests here.
+class TeamTestCase(TestCase):
+
+    fixtures = ['team_manager']
+
+#    def setUp(self):
+#        Player.objects.create(last_name="Shooter", first_name="Good")
+#        Player.objects.create(last_name="Shooter", first_name="Bad")
+#        Player.objects.create(last_name="Shooter", first_name="Tall")
+#        Player.objects.create(last_name="Shooter", first_name="Short")
+
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        #good = Player.objects.get(first_name="Good")
+        #bad = Player.objects.get(first_name="Bad")
+        #tall = Player.objects.get(first_name="Tall")
+        #short = Player.objects.get(first_name="Short")
+        players = Player.objects.all()
+        for player in players:
+    		print player.first_name
+
+        self.assertEqual(1,1)
+        self.assertEqual(2,2)
+
